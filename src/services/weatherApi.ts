03ef60ad2c961d14cb0ef230b6c656b2d3d5cbaa
Adilson Grego <adilson.gregoo@gmail.com>
2026-07-14 09:44:31 +0100

    refactor: adicionando um no endpoint para previsão dos dias

--- src/services/weatherApi.ts
+++ src/services/weatherApi.ts
@@ -3,30 +3,49 @@ const API_KEY = process.env.EXPO_PUBLIC_API_KEY!;
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 export async function getWeather(city: string) {
-    try {
-        const response = await fetch(
-            `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric&lang=pt`
-        );
+  try {
+    const response = await fetch(
+      `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric&lang=pt`,
+    );
 
-        if (!response.ok) {
-            throw new Error("Cidade não encontrada.");
-        }
-
-        const data = await response.json();
-
-        return {
-            city: data.name,
-            country: data.sys.country,
-            temperature: data.main.temp,
-            feelsLike: data.main.feels_like,
-            humidity: data.main.humidity,
-            pressure: data.main.pressure,
-            description: data.weather[0].description,
-            icon: data.weather[0].icon,
-            windSpeed: data.wind.speed,
-        };
-    } catch (error) {
-        console.error("Erro ao buscar clima:", error);
-        throw error;
+    if (!response.ok) {
+      throw new Error("Cidade não encontrada.");
     }
+
+    const data = await response.json();
+
+    return {
+      city: data.name,
+      country: data.sys.country,
+      temperature: data.main.temp,
+      feelsLike: data.main.feels_like,
+      humidity: data.main.humidity,
+      pressure: data.main.pressure,
+      description: data.weather[0].description,
+      icon: data.weather[0].icon,
+      windSpeed: data.wind.speed,
+    };
+  } catch (error) {
+    console.error("Erro ao buscar clima:", error);
+    throw error;
+  }
+}
+
+export async function getForecast(city: string) {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/forecast?q=${city}&appid=${API_KEY}&units=metric&lang=pt`,
+    );
+
+    if (!response.ok) {
+      throw new Error("Erro ao buscar previsão.");
+    }
+
+    const data = await response.json();
+
+    return data.list;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
